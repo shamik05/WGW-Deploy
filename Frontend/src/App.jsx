e6605280fbe841ea45669c929bf544a34302541b
shamik05 <64784024+shamik05@users.noreply.github.com>
2025-03-17 14:05:17 -0400

    Add mantine

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,35 +1,37 @@
+// Import styles of packages that you've installed.
+// All packages except `@mantine/hooks` require styles imports
+import '@mantine/core/styles.css';
+import { MantineProvider } from '@mantine/core';
+
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+// import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-  
-  return (
+function App() {  
+  return <MantineProvider>{  
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <header>
+    <nav>
+      <div>Home</div>
+      <div>Shopping Cart</div>
+      <div>Profile</div>
+      <div>Log Out</div>
+    </nav>
+    </header>
+
+    <main>
+      <nav>
+        <ul>Category
+          <li>Electric</li>
+          <li></li>
+        </ul>
+        
+      </nav>
+    </main>
+    <footer></footer>
+    </>  
+  }</MantineProvider>
 }
 
 export default App
